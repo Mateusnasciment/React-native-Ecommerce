11c4e476e4183c73e7f73d2dbb40d43fb470ef4a
Mateus.nasciment <106212780+Mateusnasciment@users.noreply.github.com>
2022-12-10 23:32:15 -0300

    nome

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>e-commerce</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0f6be4',
     alignItems: 'center',
     justifyContent: 'center',
   },
